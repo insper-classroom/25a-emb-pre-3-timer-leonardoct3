db0aef0d51446b85617cfc4bc78792b70d8b6078
Leonardo Teixeira <leocarvalhoteixeira@gmail.com>
2025-03-09 18:26:14 -0300

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -11,6 +11,9 @@ const int LED_PIN_G = 6;
 volatile int flag_r = 0;
 volatile int flag_g = 0;
 
+volatile int flag_timer_g = 0;
+volatile int flag_timer_r = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {
         if (gpio == BTN_PIN_R)
@@ -18,6 +21,16 @@ void btn_callback(uint gpio, uint32_t events) {
         else if (gpio == BTN_PIN_G)
             flag_g = 1;
     }
+}
+
+bool timer_g_callback(repeating_timer_t *rt) {
+    flag_timer_g = 1;
+    return true; // keep repeating    
+}
+
+bool timer_r_callback(repeating_timer_t *rt) {
+    flag_timer_r = 1;
+    return true; // keep repeating    
 }
 
 int main() {
@@ -39,14 +52,56 @@ int main() {
                                        &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
+    repeating_timer_t timer_r;
+    repeating_timer_t timer_g;
+    
+    int play_r = 0;
+    int play_g = 0;
+
     while (true) {
 
         if (flag_r) {
             flag_r = 0;
+            if (!play_r) {
+                if (!add_repeating_timer_us(500000, 
+                                    timer_r_callback,
+                                    NULL, 
+                                    &timer_r)) {
+                    printf("Failed to add RED timer\n");
+                }
+                play_r = 1;
+            } else {
+                cancel_repeating_timer(&timer_r);
+                gpio_put(LED_PIN_R, 0);
+                play_r = 0;
+            }
         }
 
         if (flag_g) {
             flag_g = 0;
+            if (!play_g) {
+                if (!add_repeating_timer_us(250000,
+                                    timer_g_callback,
+                                    NULL,
+                                    &timer_g)) {
+                    printf("Failed to add GREEN timer\n");
+                }
+                play_g = 1;
+            } else {
+                cancel_repeating_timer(&timer_g);
+                gpio_put(LED_PIN_G, 0);
+                play_g = 0;
+            }
+        }
+
+        if (flag_timer_g) {
+            gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
+            flag_timer_g = 0;
+        }
+
+        if (flag_timer_r) {
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
+            flag_timer_r = 0;
         }
     }
 }
