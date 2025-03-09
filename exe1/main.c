2ec7bfc82614bdbf20af7049bedbade9b9c49ca3
Leonardo Teixeira <leocarvalhoteixeira@gmail.com>
2025-03-09 18:14:40 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -6,12 +6,18 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int g_timer_0 = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
         flag_f_r = 1;
     } else if (events == 0x8) { // rise edge
     }
+}
+
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true; // keep repeating    
 }
 
 int main() {
@@ -26,10 +32,30 @@ int main() {
     gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                        &btn_callback);
 
-    while (true) {
+    repeating_timer_t timer_0;
 
+    int play = 0;
+
+    while (true) {
         if (flag_f_r) {
             flag_f_r = 0;
+            if (!play) {
+                if (!add_repeating_timer_us(500000, 
+                                    timer_0_callback,
+                                    NULL, 
+                                    &timer_0)) {
+                    printf("Failed to add timer\n");
+                }
+                play = 1;
+            } else {
+                cancel_repeating_timer(&timer_0);
+                play = 0;
+                gpio_put(LED_PIN_R, 0);
+            }
+        }
+        if (g_timer_0) {
+            g_timer_0 = 0;
+            gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
         }
     }
 }
